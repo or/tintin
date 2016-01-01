3a5c3a018fd733427b61df9410d68d4fc32893cd
Thialfihar <thi@thialfihar.org>
2016-01-01 01:49:45 +0100

    Upgrade cut_panels.py to Python 3, fix it

--- cut_panels.py
+++ cut_panels.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import sys
 
-import Image
+from PIL import Image
 
 PANELDIR = 'panels/'
 
-for line in file(sys.argv[1], 'r'):
+for line in open(sys.argv[1], 'r'):
     [filename, box_str] = line.split(':', 1)
     boxes = eval(box_str)
     img = Image.open(filename)
     for i, box in enumerate(boxes):
-        print box
+        print(box)
         newimg = img.crop(box)
         filename_sans_path = filename.split('/')[-1]
         newimg.save(PANELDIR + "%s_%d.jpg" % (filename_sans_path, i), quality=95)
